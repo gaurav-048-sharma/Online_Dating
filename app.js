9ccc8dddec7141720489a353a934010ff5d859ac
rohan <rohansharmas028@gmail.com>
2024-05-05 12:28:38 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = 8080;
+
+app.set("views", "ejs");
+
+app.get()
+
+app.listen(port, (req, res) => {
+    console.log(`listing on port ${port}`);
+})
